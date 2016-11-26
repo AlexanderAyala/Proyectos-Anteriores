d499ed823b86ca390161bc10d4ae0f81cad642e7
AlexanderAyala <alextankian_carpenter95@hotmail.com>
2016-11-25 19:52:30 -0600

    Ejercicio 4 Guia 2

---
+++ ejercicio4.py
@@ -0,0 +1,3 @@
+lista='La mayoria de los buenos programadores programan no porque esperan que se les pague o por adulacion por parte del publico sino porque es divertido programar'
+
+print len(lista.split())
